8dda2c5aee0f1d57b460695361bf4bad8e10a1cf
omgwtflaserguns <omgwtflaserguns@gmail.com>
2016-03-18 13:39:43 +0100

    Added Error handing in Config

--- matomat/config.py
+++ matomat/config.py
@@ -2,6 +2,11 @@ import logging
 import configparser
 from matomat.constants import Constants
 
+
+class ConfigException (Exception):
+    pass
+
+
 class Config:
 
     _parser = None
@@ -10,10 +15,16 @@ class Config:
     def __init__(self):
         self._parser = configparser.ConfigParser()
         logging.debug('Reading Config from %s' % Constants.PATH_CONFIG_FILE)
-        self._parser.read(Constants.PATH_CONFIG_FILE)
 
-        self.mongodb_uri = self._parser[Constants.CONFIG_SECTION_GENERAL][Constants.CONFIG_ATTRIBUTE_MONGODB_URI]
-        logging.debug('Config - %s -> %s' % (Constants.CONFIG_ATTRIBUTE_MONGODB_URI, self.mongodb_uri))
+        try:
+            self._parser.read(Constants.PATH_CONFIG_FILE)
+
+            self.mongodb_uri = self._parser[Constants.CONFIG_SECTION_GENERAL][Constants.CONFIG_ATTRIBUTE_MONGODB_URI]
+            logging.debug('Config - %s -> %s' % (Constants.CONFIG_ATTRIBUTE_MONGODB_URI, self.mongodb_uri))
+        except Exception as e:
+            msg = 'Could not read Config file from %s Config structure can be seen in example.conf' % Constants.PATH_CONFIG_FILE
+            logging.fatal(msg)
+            raise ConfigException(msg) from e
 
 
 
