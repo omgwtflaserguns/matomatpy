c0cc1d396cf292193b773a8aaad1813686b6afd7
omgwtflaserguns <omgwtflaserguns@gmail.com>
2016-03-23 13:32:50 +0100

    Added menu selection per arrow keys

--- matomat/ui/menu.py
+++ matomat/ui/menu.py
@@ -1,4 +1,5 @@
 import pyfiglet
+import curses
 from matomat.ui.form import FormBase
 from matomat.models.point import Point
 
@@ -12,6 +13,7 @@ class MenuForm (FormBase):
         self.figlet = pyfiglet.Figlet()
         self.colors = colors
         self.menuitems = None
+        self.currentItem = 0
 
     def set_items(self, menu_items):
         self.menuitems = menu_items
@@ -24,15 +26,48 @@ class MenuForm (FormBase):
 
     def _draw_menu(self, screen):
         y = MenuForm.MENU_POSITION.y
+        index = 0
         for item in self.menuitems:
-            screen.addstr(y, MenuForm.MENU_POSITION.x, item.title)
+            if index == self.currentItem:
+                screen.addstr(y, MenuForm.MENU_POSITION.x, item.title, curses.A_STANDOUT)
+            else:
+                screen.addstr(y, MenuForm.MENU_POSITION.x, item.title)
             y += 1
+            index += 1
+
+    def list_up(self):
+        if self.currentItem == 0:
+            self.currentItem = len(self.menuitems) - 1
+        else:
+            self.currentItem -= 1
+
+    def list_down(self):
+        if self.menuitems:
+            if self.currentItem == len(self.menuitems) - 1:
+                self.currentItem = 0
+            else:
+                self.currentItem += 1
 
     def show(self, screen):
-        screen.clear()
-        self._draw_header(screen)
-        self._draw_menu(screen)
-        screen.getch()
+        self.currentItem = 0
+
+        while True:
+            screen.clear()
+            curses.curs_set(0)
+            self._draw_header(screen)
+            self._draw_menu(screen)
+            key = screen.getch()
+
+            if key == curses.KEY_ENTER:
+                return self.menuitems[self.currentItem]
+            elif key == ord('q'):
+                return None
+            elif key == curses.KEY_DOWN:
+                self.list_down()
+            elif key == curses.KEY_UP:
+                self.list_up()
+
+
 
 
 
