f7c9b75fb49ead33f6ef17eea25b45eac47709d8
omgwtflaserguns <omgwtflaserguns@gmail.com>
2016-04-20 16:41:40 +0200

    Added unittests for authorization service

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -8,14 +8,14 @@ from matomat.models.menu import MenuKey
 
 class TestAuthorization(unittest.TestCase):
 
-    dummy_user = {'username': 'DummyUser', 'password': 'dummypassword'}
+    dummy_user = {'username': 'DummyUser', 'password': 'dummypassword', 'rights': ['bb', 'of']}
 
     def setUp(self):
 
         self.box = MongoBox()
         self.box.start()
         db = self.box.client().matomat
-        db.users.insert({'username': 'DummyUser', 'password': 'dummypassword'})
+        db.users.insert(self.dummy_user)
 
         self.sut = Authorization(db, MicroMock.get_log_mock())
 
@@ -24,7 +24,7 @@ class TestAuthorization(unittest.TestCase):
 
     def test_should_set_logged_in_user(self):
         self.assertTrue(self.sut.login('DummyUser', 'dummypassword'))
-        self.assertTrue(set(self.dummy_user.items()).issubset(set(self.sut.currentUser.items())))
+        self.assertTrue(self.dummy_user == self.sut.currentUser)
 
     def test_should_not_login_wrong_username(self):
         self.assertFalse(self.sut.login('wrongUser', 'dummypassword'))
@@ -33,6 +33,14 @@ class TestAuthorization(unittest.TestCase):
     def test_should_not_login_wrong_password(self):
         self.assertFalse(self.sut.login('DummyUser', 'wrongPassword'))
         self.assertTrue(self.sut.currentUser is None)
+
+    def test_sould_return_true_on_has_right(self):
+        self.sut.login('DummyUser', 'dummypassword')
+        self.assertTrue(self.sut.user_has_right('bb'))
+
+    def test_sould_return_false_on_has_not_right(self):
+        self.sut.login('DummyUser', 'dummypassword')
+        self.assertFalse(self.sut.user_has_right('omg'))
 
     def test_main_menu_should_not_contain_prohibited_Entries(self):
         menu = self.sut.create_main_menu()
